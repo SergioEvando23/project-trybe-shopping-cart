960b0161db770f4c0538f8bd6fddaf234461c91f
SergioEvando23 <sergioevandocosta@gmail.com>
2022-01-18 16:17:00 -0300

    removendo erro de es lint

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ function calculateTotal() {
   const total = cartItems.reduce((accumulator, item) => accumulator + item.salePrice, 0);
   console.log(total);
   totalInCart.innerText = `${total}`;
-
 }
 
 function getSkuFromProductItem(item) {
